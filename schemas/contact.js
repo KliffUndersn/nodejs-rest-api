8587b210e7201ec789c72a65948760752e06b041
KliffUndersn <kliffundersn90@gmail.com>
2021-11-21 15:57:43 +0200

    hwdone

--- schemas/contact.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const joiSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email({ tlds: { allow: false } }),
-  phone: Joi.string().required(),
-});
-
-module.exports = joiSchema;
